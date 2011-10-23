101f02d10298d24de3f87c6dce0a5c03b6c8e94c
g00chy <totugekiluna@gmail.com>
2011-10-23 18:12:34 +0900

    delete settings

--- settings/GAE(windows)/appcfg_update.bat
+++
@@ -1,2 +0,0 @@
-cd /d "%~dp1"
-C:\Python25\python.exe "C:\Program Files\Google\goole_appengine\appcfg.py" update "%~n1"
--- settings/GAE(windows)/dev_apserver.bat
+++
@@ -1,2 +0,0 @@
-cd /d "%d~ dp1"
-C:\Python25\python.exe "C:Program Files\Google\google_appengine\dev_appserver.py" "%~n1"
