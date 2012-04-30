d85f74840a5333bd8c4135becd3a31b46c29b923
g00chy <totugekiluna@gmail.com>
2012-04-30 20:29:47 +0900

    commit php

---
+++ lang/php/mydns.php
@@ -0,0 +1,15 @@
+<?php
+#require_once ('pp.class');
+    $url = 'http://www.mydns.jp/login.html';
+    $masterid = 'mydns90546';
+    $masterpwd = 'hX3iX2j8';
+    file_get_contents("j
+    var_dump ($contents);
+    option = array(
+      "fimeout" => "10",
+      "allowRedirects" => true,
+      "maxRedirects" => 3,
+    );
+    $http = new HTTP_Request($url,$option);
+    $http->addHeader("
+?>
