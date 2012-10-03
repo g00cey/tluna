570b25bd9fcb944c9e7e820ce617dc661a868197
g00chy <totugekiluna@gmail.com>
2012-10-03 23:48:04 +0900

    deleted

--- lang/ruby/bloggertomixi/mixi.rb
+++
@@ -1,6 +0,0 @@
-#!/Users/luna/.rvm/bin/ruby
-
-# -*- coding: utf-8 -*-
-require 'mechanize'
-
-#このプログラムはmixiの処理をまとめたところ
--- lang/ruby/bloggertomixi/sitemanager.rb
+++
@@ -1,9 +0,0 @@
-#!/Users/luna/.rvm/bin/ruby
-
-# -*- coding: utf-8 -*-
-#このプログラムはbloggerの処理をまとめる
-
-require 'active_record'
-#サイト管理用のクラス
-
-
