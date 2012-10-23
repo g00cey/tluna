cfcdf83db963951feeb4610739915b4fdbec3987
g00chy <totugekiluna@gmail.com>
2012-10-24 00:30:39 +0900

    delete gsearch

--- cgi-bin/gsearch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-google_q='http://google.co.jp/search?btnG=Google&q'
-
-cat <<_END_
-Content-type: text/plain
-W3m-control: GOTO $google_q=$QUERY_STRING
-W3m-control: DELETE_PREVBUF
-
-_END_
