961a5e9674e8d7d77abd12093640f94ac183b49c
g00chy <totugekiluna@gmail.com>
2015-06-07 05:57:46 +0900

    changed

---
+++ environments/windows/vim-setup.bat
@@ -0,0 +1,9 @@
+REM vimrcsetup
+REM symboliclink vimrc
+mklink /H %HOMEPATH%\_vimrc    %HOMEPATH%\src\git\github\tluna\vim\vimrc   
+mklink /H %HOMEPATH%\_gvimrc   %HOMEPATH%\src\git\github\tluna\vim\gvimrc  
+mklink /D %HOMEPATH%\_vimfiles %HOMEPATH%\src\git\github\tluna\vim         
+REM bundle
+cd %HOMEPATH%\src\git\github\tluna
+git submodule init
+git submodule update
