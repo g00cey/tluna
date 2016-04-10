3861d846a76782d60f1161994a90c3c7efdbd98c
g00chy <totugekiluna@gmail.com>
2016-04-11 01:14:19 +0900

    added

---
+++ bin/rsync.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+rsync -av /Volumes/3tv1               /Volumes/3tv2/
+rsync -av /Volumes/3tv1/Documents/    /Volumes/ouhdd/
+rsync -av /Volumes/3tv1/pic/          /Volumes/ouhdd/
+rsync -av /Volumes/3tv1/iTunes/       /Volumes/ouhdd/
+rsync -av /Volumes/3tv1/Documents/    /Volumes/ouhdd/
+rsync -av /Volumes/3tv1/movie/        /Volumes/ouhdd/
+rsync -av /Volumes/3tv1/rtorrent/     /Volumes/ouhdd/
+rsync -av /Volumes/3tv1/vagrant/      /Volumes/ouhdd/
