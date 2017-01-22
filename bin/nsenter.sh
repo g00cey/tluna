850436dc41ab73ac8cf4666e44a4a4ea589a56b0
g00chy <totugekiluna@gmail.com>
2017-01-22 12:07:13 +0900

    commit

--- bin/nsenter.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-docker ps -a
-echo 'puts container name'
-read container
-#sudo nsenter -t $(sudo docker inspect --format '{{.State.Pid}}' $container) -m -u -i -n
-sudo nsenter -t $(sudo docker inspect --format '{{.State.Pid}}' $container) -u -i -n
