cdb4fd019e2a961558bdec88eaa0101fc2650826
g00chy <totugekiluna@gmail.com>
2019-05-02 20:49:55 +0900

    add environment and change vimrc

---
+++ environments/windows/setup.ps1
@@ -0,0 +1,4 @@
+# Start-Process powershell -Verb runas setup.ps1
+New-Item -Value C:\Users\luna\workspace\git\github\tluna\vim  -Path C:\Users\luna\ -Name _vim -ItemType symboliclink
+New-Item -Value C:\Users\luna\workspace\git\github\tluna\vim\gvimrc  -Path C:\Users\luna\ -Name _gvimrc -ItemType symboliclink
+New-Item -Value C:\Users\luna\workspace\git\github\tluna\vim\vimrc  -Path C:\Users\luna\ -Name _vimrc -ItemType symboliclink
--- windows/keyload.bat
+++
@@ -1,2 +0,0 @@
-"C:\Users\luna\soft\putty-0.60-jp20070603\pageant.exe" "C:\Users\luna\Dropbox\key\tluna_id_rsa.ppk" "C:\Users\luna\Dropbox\key\git_id_rsa.ppk" "C:\Users\luna\Dropbox\key\ubuntu_local.ppk" 
-EXIT
