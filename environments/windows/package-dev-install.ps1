54be168cc3d9c103d670e192b821ddf37f4ef497
g00chy <totugekiluna@gmail.com>
2020-06-06 17:59:42 +0900

    add gaming install

---
+++ environments/windows/package-dev-install.ps1
@@ -0,0 +1,2 @@
+choco install -y dev.config
+choco install -y --ignorechecksum dev.config
--- environments/windows/package-install.ps1
+++
@@ -1,3 +0,0 @@
-choco install -y dev.config
-choco install -y --ignorechecksum dev.config
-choco install -y gaming.config
