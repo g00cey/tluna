3eca51b700cdd2a4a9e5a8339577814b58398dc6
g00chy <totugekiluna@gmail.com>
2020-11-15 17:41:44 +0900

    c

--- bin/dualscreen-rdp.sh
+++ bin/dualscreen-rdp.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-xfreerdp /multimon /u:luna /audio-mode:1 /f +fonts +clipboard /p:totugeki /v:192.168.0.10
+xfreerdp /multimon /u:${XRDP_USER} /audio-mode:1 /f +fonts +clipboard /p:${XRDP_PASS} /v:192.168.0.10
