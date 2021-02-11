aa598271b946b78b6f823c4057d17e451d5f6e9c
g00chy <totugekiluna@gmail.com>
2021-02-12 05:49:12 +0900

    add: wsl2 desktop

---
+++ environments/linux/wsl2-desktop.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo '
+# wsl2-desktop.sh added
+export PATH="$HOME/.anyenv/bin:$PATH"
+eval "$(anyenv init -)"
+
+export GTK_IM_MODULE=fcitx
+export QT_IM_MODULE=fcitx
+export XMODIFIERS=\"@im=fcitx\"
+export DefaultIMModule=fcitx
+if [ $SHLVL = 1 ] ; then
+  xset -r 49  > /dev/null 2&>1
+  (fcitx-autostart > /dev/null 2&>1 &)
+fi
+
+export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '\''{print $2}'\''):0.0
+# wsl2-desktop.sh added
+' >> ~/.profile
+
+sudo apt-get -y install fcitx-mozc fonts-noto-cjk dbus-x11 language-pack-ja
+sudo sed -i -e 's/# ja_JP.UTF-8/ja_JP.UTF-8/' /etc/locale.gen
+sudo locale-gen
+sudo update-locale LANG=ja_JP.UTF-8
+
+source ~/.profile
