04ab3f9901a932980fd2903ccc6db4ad9c1dffe0
g00chy <totugekiluna@gmail.com>
2021-02-13 15:00:17 +0900

    add: package change: XMODIFIER in profile

--- environments/linux/wsl2-desktop.sh
+++ environments/linux/wsl2-desktop.sh
@@ -7,7 +7,7 @@ eval "$(anyenv init -)"
 
 export GTK_IM_MODULE=fcitx
 export QT_IM_MODULE=fcitx
-export XMODIFIERS=\"@im=fcitx\"
+export XMODIFIERS=@im=fcitx
 export DefaultIMModule=fcitx
 if [ $SHLVL = 1 ] ; then
   xset -r 49  > /dev/null 2&>1
@@ -18,7 +18,7 @@ export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '\''{print $2}'\''
 # wsl2-desktop.sh added
 ' >> ~/.profile
 
-sudo apt-get -y install fcitx-mozc fonts-noto-cjk dbus-x11 language-pack-ja
+sudo apt-get -y install fcitx-mozc fonts-noto-cjk dbus-x11 language-pack-ja fonts-noto-color-emoji fonts-symbola
 sudo sed -i -e 's/# ja_JP.UTF-8/ja_JP.UTF-8/' /etc/locale.gen
 sudo locale-gen
 sudo update-locale LANG=ja_JP.UTF-8
