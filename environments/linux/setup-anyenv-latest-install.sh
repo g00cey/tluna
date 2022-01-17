7016ec80f27479e8da98e5d1b5df07239e8998b0
g00chy <totugekiluna@gmail.com>
2022-01-17 20:45:14 +0000

    change

--- environments/linux/setup-anyenv-latest-install.sh
+++ environments/linux/setup-anyenv-latest-install.sh
@@ -1,4 +1,13 @@
 #!/bin/zsh
+pyversion=`pyenv install --list | egrep ' [0-9]+.*' | egrep -v '[a-z]' | tail -1 | sed 's/ //g'`
+echo $pyversion
+goversion=`goenv install --list | egrep ' [0-9]+.*' | egrep -v '[a-z]' | tail -1 | sed 's/ //g'`
+echo $goversion
+phpversion=`phpenv install --list | egrep ' [0-9]+.*' | egrep -v '[a-z]' | tail -1 | sed 's/ //g'`
+echo $phpversion
+nodeversion=`nodenv install --list | egrep '[1-9]+.*' | egrep -v '[a-z]' | tail -1 | sed 's/ //g'`
+echo $nodeversion
+
 pyenv install $pyversion
 goenv install $goversion
 phpenv install $phpversion
