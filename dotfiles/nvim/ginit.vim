d92aa651f435ba1fc37d36766fa6958914d36dd8
g00chy <totugekiluan@gmail.com>
2022-04-10 18:12:11 +0900

    change

---
+++ dotfiles/nvim/ginit.vim
@@ -0,0 +1,1 @@
+set guifont=Cica:10
--- environments/windows/setup.ps1
+++ environments/windows/setup.ps1
@@ -1,11 +1,39 @@
 # Start-Process powershell -Verb runas setup.ps1
-$localDir  = $Env:LOCALAPPDATA + "\nvim"
+$localDir = $Env:LOCALAPPDATA + "\nvim"
 If(-not (Test-Path $localDir)) {
     mkdir $Env:LOCALAPPDATA/nvim
 }
+# local
 $initvim = Resolve-Path ..\..\dotfiles\nvim\init.vim
-If(-not (Test-Path $initvim)) {
+$localInitVim = $localDir + "\init.vim"
+If(-not (Test-Path $localInitVim)) {
     New-Item -Value $initvim -Path $localDir -Name init.vim -ItemType symboliclink
+}
+$ginitvim = Resolve-Path ..\..\dotfiles\nvim\ginit.vim
+$localGinitVim = $localDir + "\ginit.vim"
+If(-not (Test-Path $localGinitVim)) {
+    New-Item -Value $ginitvim -Path $localDir -Name ginit.vim -ItemType symboliclink
+}
+
+$configDir = $Env:HOMEPATH + "\.config"
+# config
+If(-not (Test-Path $configDir)) {
+    mkdir $configDir
+}
+$configNvimDir = $configDir + "\nvim"
+If(-not (Test-Path $configNvimDir)) {
+    mkdir $configNvimDir
+}
+$configNvimPlugSettingDir = $configNvimDir + "\plug-setting"
+$plugSetting = Resolve-Path ..\..\dotfiles\nvim\plug-setting
+If(-not (Test-Path $configNvimPlugSettingDir)) {
+    New-Item -Value $plugSetting -Path $configNvimDir -Name "plug-setting" -ItemType symboliclink
+}
+
+$configNvimKeymap = $configNvimDir + "\keymap.vim"
+$nvimKeymap= Resolve-Path ..\..\dotfiles\nvim\keymap.vim
+If(-not (Test-Path $configNvimKeymap)) {
+    New-Item -Value $nvimKeymap -Path $configNvimDir -Name "keymap.vim" -ItemType symboliclink
 }
 
 $localAutoDir = $localDir + "\autoload"
