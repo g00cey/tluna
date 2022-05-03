e1697ac219bbe782ab0680225b36fac2a0e6dcfb
g00chy <totugekiluna@gmail.com>
2022-05-04 05:28:39 +0900

    change

--- environments/linux/setup-dconf.sh
+++ environments/linux/setup-dconf.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sudo apt install -y dconf-editor
+sudo apt install -y dconf-editor dconf-cli
 dconf load /org/gnome/desktop/wm/keybindings/ < ~/src/git/github/tluna/dotfiles/dconf/keybindings
 
