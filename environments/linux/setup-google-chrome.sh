14e8d02e1bbb2fd5ef39b7f2acc50aa9fe9ca2a3
g00chy <totugekiluna@gmail.com>
2022-06-10 20:04:57 +0000

    c

--- environments/linux/setup-google-chrome.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ ! -e '/etc/apt/sources.list.d/google.list' ]; then
-  sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-fi
-
-sudo wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo apt update
-sudo apt install google-chrome-stable
--- environments/linux/setup-nvim.sh
+++ environments/linux/setup-nvim.sh
@@ -4,7 +4,3 @@ sudo snap install nvim --classic
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-directory=`dirname $0`
-$directory/setup-anyenv.sh
-$directory/setup-anyenv-latest-install.sh
-pip3 install jedi-language-server
