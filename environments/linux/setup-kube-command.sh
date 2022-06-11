8417ff9c494da3da3b9a61b66256622035f1cfde
g00chy <totugekiluna@gmail.com>
2022-06-11 09:21:09 +0000

    fix kubectl download

--- environments/linux/setup-kube-command.sh
+++ environments/linux/setup-kube-command.sh
@@ -2,7 +2,7 @@
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 -o /tmp/get
 chmod u+x /tmp/get
 sudo /tmp/get
-curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl" -o /tmp/kubectl
+curl -L "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl" -o /tmp/kubectl
 chmod au+x /tmp/kubectl
 sudo mv /tmp/kubectl /usr/local/bin/
 
