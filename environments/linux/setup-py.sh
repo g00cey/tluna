11459e2e75819185dc02fea76bf5f1d08b27e769
g00chy <totugekiluna@gmail.com>
2022-09-10 18:17:53 +0000

    changed

--- environments/linux/setup-py.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-directory=`dirname $0`
-$directory/setup-anyenv.sh
-$directory/setup-anyenv-latest-install.sh
-
-pip3 install python-language-server
-pip3 install pyls-isort
-pip3 install pyls-black
