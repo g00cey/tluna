9a58037cd6016df77f1fe0f363e3b827d429749c
g00chy <totugekiluna@gmail.com>
2022-09-12 00:06:05 +0900

    change

--- environments/linux/setup-nvim.sh
+++ environments/linux/setup-nvim.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-#setup-nvim
-sudo snap install nvim --classic
+sudo add-apt-repository -y ppa:neovim-ppa/stable
+sudo apt update
+sudo apt install -y neovim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
