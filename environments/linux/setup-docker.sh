3d7c21a5f26923ac36ece95e8bcb4de5fab2aaad
g00chy <totugekiluna@gmail.com>
2022-09-22 14:17:11 +0000

    update
    dockerインストール方法を更新

--- environments/linux/setup-docker.sh
+++ environments/linux/setup-docker.sh
@@ -1,39 +1,36 @@
 #!/bin/bash
-
-function ubunt-install {
-	sudo apt-get install \
-	    ca-certificates \
-	    curl \
-	    gnupg \
-	    lsb-release
-	sudo mkdir -p /etc/apt/keyrings
-	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-	echo \
-	  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-	  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-	sudo apt update
-	sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+function install-ubuntu() {
+  sudo apt-get install \
+      ca-certificates \
+      curl \
+      gnupg \
+      lsb-release
+  sudo mkdir -p /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+  echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo apt update
+  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 }
-
-function debian-install {
-	sudo apt update
-	sudo apt-get install \
-	    ca-certificates \
-	    curl \
-	    gnupg \
-	    lsb-release
-	sudo mkdir -p /etc/apt/keyrings
-	curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-	echo \
+function install-debian() {
+  sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+  sudo mkdir -p /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+  echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-	sudo apt-get update
-	sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+  sudo apt-get update
+  sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 }
 
-count=`uname -a | grep Debian | wc -l`
-if [ $count = 1 ]; then
-	debian-install
+ubuntu=`uname -a | grep Ubuntu | wc -l`
+if [ $ubuntu -eq 1 ]; then
+  install-ubuntu
 else
-	ubuntu-install
+  install-debian
 fi
