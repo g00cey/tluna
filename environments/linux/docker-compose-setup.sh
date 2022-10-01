4d43f86bc5c2e62177d8a42ffce1ff25e9167cd4
g00chy <totugekiluna@gmail.com>
2022-10-01 15:54:33 +0000

    change

--- environments/linux/docker-compose-setup.sh
+++ environments/linux/docker-compose-setup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-curl https://api.github.com/repos/docker/compose/releases/latest  | jq '. | .assets[] | select(.name == "docker-compose-linux-x86_64") | .browser_download_url' | xargs -I {} curl {} -OL ./docker-compose
-chmod +x docker-compose-linux-x86_64
-mv docker-compose-linux-x86_64 docker-compose
+curl https://api.github.com/repos/docker/compose/releases/latest  | jq '. | .assets[] | select(.name == "docker-compose-linux-x86_64") | .browser_download_url' | xargs -I {} curl -L -o ./docker-compose {}
+chmod +x docker-compose
 sudo mv docker-compose /usr/local/bin/
