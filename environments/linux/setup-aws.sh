99015dff4ea1921190de002a9b89d3c61bef82f8
g00cey <totugekiluna@gmail.com>
2023-04-28 14:21:04 +0900

    changed

--- environments/linux/setup-aws.sh
+++ environments/linux/setup-aws.sh
@@ -1,4 +1,10 @@
+<<<<<<< HEAD
 #!/bin/zsh
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/tmp/aws-cli.zip"
 unzip /tmp/aws-cli.zip -d /tmp/aws-cli
 sudo /tmp/aws-cli/aws/install --update
+=======
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/tmp/awscliv2.zip"
+unzip /tmp/awscliv2.zip -d /tmp/awscliv2
+sudo /tmp/awscliv2/aws/install
+>>>>>>> 55a2f63 (changed)
