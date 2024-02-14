b59eafeeba98f4384b2d1a7e3de2a76874651e63
g00cey <totugekiluna@gmail.com>
2024-02-15 02:02:49 +0900

    change

--- dotfiles/nvim/lua/plug-setting/language-server.lua
+++ dotfiles/nvim/lua/plug-setting/language-server.lua
@@ -1,12 +1,3 @@
-vim.cmd([[autocmd BufWritePost init.lua source <afile> | PackerCompile]])
-
-require('mason').setup {
-  ui = {
-    check_outdated_packages_on_open = false,
-    border = 'single',
-  },
-}
-
 local on_attach = function(client, bufnr)
   -- LSPサーバーのフォーマット機能を無効にするには下の行をコメントアウト
   -- 例えばtypescript-language-serverにはコードのフォーマット機能が付いているが代わりにprettierでフォーマットしたいときなどに使う
@@ -32,17 +23,42 @@ local on_attach = function(client, bufnr)
   set("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>")
 end
 
-require('mason').setup {
-  ui = {
-    check_outdated_packages_on_open = false,
-    border = 'single',
-  },
-}
-
--- ここに追記
-require('mason-lspconfig').setup_handlers {
-  function(server_name)
-    require('lspconfig')[server_name].setup {}
+require("mason").setup()
+require("mason-lspconfig").setup()
+require("mason-lspconfig").setup_handlers {
+  function (server_name)
+    require("lspconfig")[server_name].setup {
+      on_attach = on_attach
+    }
   end,
 }
 
+-- lspのハンドラーに設定
+capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+-- lspの設定後に追加
+vim.opt.completeopt = "menu,menuone,noselect"
+
+local cmp = require"cmp"
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      require("luasnip").lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<C-p>"] = cmp.mapping.select_prev_item(),
+    ["<C-n>"] = cmp.mapping.select_next_item(),
+    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.close(),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+  }),
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+    { name = "luasnip" },
+  }, {
+    { name = "buffer" },
+  })
+})
