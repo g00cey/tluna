e95c378f408eb5ab1e176d301e888d292be7dec8
g00cey <totugekiluna@gmail.com>
2024-10-06 00:05:22 +0900

    update: 最後にexportするように

--- environments/linux/setup-kube-command.sh
+++ environments/linux/setup-kube-command.sh
@@ -12,3 +12,14 @@ tar xvzf /tmp/helmfile.tar.gz helmfile
 chmod +x helmfile
 sudo mv helmfile /usr/local/bin/
 helm plugin install https://github.com/databus23/helm-diff
+
+# install krew
+(
+  set -x; cd "$(mktemp -d)" &&
+  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+  KREW="krew-${OS}_${ARCH}" &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
+  tar zxvf "${KREW}.tar.gz" &&
+  ./"${KREW}" install krew
+)
