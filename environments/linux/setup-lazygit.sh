c08a0939cc8a08735760ca79bb2134ad0b9c50f9
goocey <totugekiluna@gmail.com>
2025-09-25 16:07:02 +0900

    change

--- environments/linux/setup-lazygit.sh
+++ environments/linux/setup-lazygit.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-go install github.com/jesseduffield/lazygit@latest
+#go install github.com/jesseduffield/lazygit@latest
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+rm lazygit.tar.gz
+sudo install lazygit -D -t /usr/local/bin/
+rm lazygit
