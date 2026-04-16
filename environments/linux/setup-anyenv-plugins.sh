8fafab5775bfae9d922b2aa2d0048cfc557b55a2
g00cey <totugekiluna@gmail.com>
2026-04-16 12:33:28 +0900

    change

--- environments/linux/setup-anyenv-plugins.sh
+++ environments/linux/setup-anyenv-plugins.sh
@@ -4,6 +4,7 @@
 mkdir -p "$(nodenv root)/plugins"
 git clone https://github.com/pine/nodenv-yarn-install.git "$(nodenv root)/plugins/nodenv-yarn-install"
 nodenv hooks install
+git clone https://github.com/nodenv/nodenv-update.git "$(nodenv root)"/plugins/nodenv-update
 
 # anyenv update
 mkdir -p $(anyenv root)/plugins
@@ -13,3 +14,4 @@ anyenv update
 # pyenv virtualenv-init
 mkdir -p $(anyenv root)/plugins
 git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
+
